a3d34e525534aee5577ceb6180da6a1e15312047
Dean Bale <dean@baleuk.org>
2015-10-13 20:07:10 +0100

    trial

--- timed_response.py
+++ timed_response.py
@@ -4,52 +4,88 @@ Created on Tue Oct 13 15:11:02 2015
 
 @author: Dean
 """
+"""
+http://chimera.labs.oreilly.com/books/1230000000393/ch12.html#_solution_197
 
+class CountdownTask:
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self, n):
+        while self._running and n > 0:
+            print('T-minus', n)
+            n -= 1
+            time.sleep(5)
+
+c = CountdownTask()
+t = Thread(target=c.run, args=(10,))
+t.start()
+...
+c.terminate() # Signal termination
+t.join()      # Wait for actual termination (if needed)
+"""
 ###http://stackoverflow.com/questions/14029548/input-with-time-limit-countdown
 
 import threading
 import time
-import os
 
-def timed_response():
+class timed_response():
+    
+    timed_out = False    
+    
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self, seconds, increment):
+        timed_out = False
+        second = seconds
+        while self._running and second > 0:
+            if seconds == second:
+                print()
+            print("You have " + str(second) + " seconds left: " )
+            if second == 0:
+                timed_out = True
+                break
+            time.sleep(increment)
+            second -= increment
+            
+
+#    def skip_input_after_timout(seconds,increment):
+#        """
+#        Threading function, after N seconds print something and exit program
+#        """
+#        for second in range(seconds,-1,-increment):
+#            time.sleep(increment)
+#            if seconds == second:
+#                print()
+#                print("You have " + str(second) + " seconds left: " )
+#                print("You did not enter a command quick enough!")
+#                pass
+
+def response():
     """
     Simple function where you ask him his name, if he answers
     you print message and exit
     """
-    response = input("Enter you decision quickly!!!:")
-    return response
-
-def exit(msg):
-    """
-    Exit function, prints something and then exits using OS
-    Please note you cannot use sys.exit when threading..
-    You need to use os._exit instead
-    """
-    print(msg)
-    
-    #os._exit(1)
-
-def skip_input_after_timout(seconds,increment):
-    """
-    Threading function, after N seconds print something and exit program
-    """
-    for second in range(seconds,-1,-increment):
-        time.sleep(increment)
-        if seconds == second:
-            print()
-        print("You have " + str(second) + " seconds left: " )
-    print("You did not enter a command quick enough!")
-    pass
+    R = input("Enter you decision quickly!!!:")
+    return R
 
 def quick_response():
+    t_r = timed_response()
     # define skip_input_after_timout as a threading function, 5 as an argument
-    quick_response_thread = threading.Thread(target=skip_input_after_timout,args=(10,2,))
+    quick_response_thread = threading.Thread(target=t_r.run,args=(10,2,))
     quick_response_thread.setDaemon(True)
     # start threading
     quick_response_thread.start()
     # ask him his name
     N = None
-    N = timed_response()
+    N = response()
     if N != None:
         #quick_response_thread._stop
         print("thank you")
