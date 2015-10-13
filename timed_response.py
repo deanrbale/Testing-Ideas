4b61672e7c0d0db5e8531ee18b8a90429e4a58a3
Dean Bale <dean@baleuk.org>
2015-10-13 22:03:43 +0100

    I think the timed response

--- timed_response.py
+++ timed_response.py
@@ -6,33 +6,15 @@ Created on Tue Oct 13 15:11:02 2015
 """
 """
 http://chimera.labs.oreilly.com/books/1230000000393/ch12.html#_solution_197
-
-class CountdownTask:
-    def __init__(self):
-        self._running = True
-
-    def terminate(self):
-        self._running = False
-
-    def run(self, n):
-        while self._running and n > 0:
-            print('T-minus', n)
-            n -= 1
-            time.sleep(5)
-
-c = CountdownTask()
-t = Thread(target=c.run, args=(10,))
-t.start()
-...
-c.terminate() # Signal termination
-t.join()      # Wait for actual termination (if needed)
+http://stackoverflow.com/questions/14029548/input-with-time-limit-countdown
 """
-###http://stackoverflow.com/questions/14029548/input-with-time-limit-countdown
-
+#import relevent packages
 import threading
 import time
 
 class timed_response():     
+    
+    valid_input = True   
     
     def __init__(self):
         self._running = True
@@ -41,6 +23,7 @@ class timed_response():
         self._running = False
         
     def run_response(self):
+        #While the thread is running accept a response is not yet been given ask for one
         response = ""
         while self._running and response == "":
              response = str(input("Enter your decision quickly: "))
@@ -48,40 +31,55 @@ class timed_response():
 
     def run_timer(self, seconds, increment):
         second = seconds
+        #Pause to allow for the run_response to start
         time.sleep(1)
+        #While the thread is running and the counter has not reached zero
         while self._running and second >= 0:
             if seconds == second:
                 print()
+            #Display time left
             print("You have " + str(second) + " seconds left! " )
             print()
+            #Check whether time has ran out
             if second == 0:
-                break
-                self._running = False
+                #Give an appropiate command
+                print("You were too slow, press ENTER to see what happens to you now.")
+                self.valid_input = False
+                #Terminate thread
                 self.terminate()
+            #Pause for increment amount of time
             time.sleep(increment)
+            #Reduce time left
             second -= increment
 
-def quick_response():
-    t_r = timed_response()
-    # define skip_input_after_timout as a threading function, 5 as an argument
-    quick_response_thread = threading.Thread(target=t_r.run_timer,args=(10,2,))
+def quick_response(max_time,increment):
+    timed_response_class = timed_response()
+    # define timed_response_class.run_timer as a threading function
+    quick_response_thread = threading.Thread(target=timed_response_class.run_timer,args=(max_time,increment,))
+    #set the thread to daemon mode
     quick_response_thread.setDaemon(True)
     # start threading
     quick_response_thread.start()
-    # ask him his name
-    R = str(t_r.run_response())
+    # ask user for action (may need to take loop from function and place here when validation checks are present)
+    action = str(timed_response_class.run_response())
+    #If the thread is still running, the user has entered a valid action (need valid check here)
     if quick_response_thread.is_alive():
-        #quick_response_thread._stop
-        t_r.terminate()
+        #terminate the thread
+        timed_response_class.terminate()
+        #wait for actual termination
         quick_response_thread.join()
-        print("thank you")
-    else:
-        print("too late")
-    input("")
+    #check to see if there was a valid input with the time
+    if not timed_response_class.valid_input:
+        action = ""
+    return action
     
-
-quick_response()
-
+#test
+response = ""
+response = quick_response(15,3)
+if response != "":
+    print("thank you")
+else:
+    print("oh no")
 
    
    
